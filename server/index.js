a792178d6c14c01bb697a29e2db0b4638348d547
Anurag Dangi <anuragrajpoot2468@gmail.com>
2025-03-28 20:58:48 +0530

    create basic running app

---
+++ server/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const dbConnect = require("./src/configs/database");
+// const fileUpload = require("express-fileupload");
+// const {cdConnect} = require("./server/utils/fileUploader")
+// const mailSender = require("./server/utils/mailSender")
+
+// const authRoutes = require("./server/routes/auth");
+// const additionalDetailsRoutes = require("./server/routes/additionalDetails");
+// const courseRoutes = require("./server/routes/course");
+// const categoryRoutes = require("./server/routes/category");
+// const sectionRoutes = require("./server/routes/section");
+// const subSectionRoutes = require("./server/routes/subSection");
+// const ratingandReviewRoutes = require("./server/routes/ratingAndReview");
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+	cors({
+		origin: "*",
+		credentials: true,
+	})
+);
+// app.use(
+// 	fileUpload({
+// 		useTempFiles: true,
+// 		tempFileDir: "/tmp/",
+// 	})
+// );
+
+dbConnect();
+// cdConnect();
+
+app.listen(process.env.PORT,()=>{
+    console.log(`running at PORT ${process.env.PORT}`)
+})
+
+// app.use("/api/v1/auth", authRoutes);
+// app.use("/api/v1/additionalDetails", additionalDetailsRoutes);
+// app.use("/api/v1/course", courseRoutes);
+// app.use("/api/v1/category", categoryRoutes);
+// app.use("/api/v1/section", sectionRoutes);
+// app.use("/api/v1/subSection", subSectionRoutes);
+// app.use("/api/v1/ratingAndReview", ratingandReviewRoutes);
+
+app.get("/",(req,res)=>{
+    res.send(`<h1>running at PORT ${process.env.PORT}</h1>`)
+})
+
+
+
